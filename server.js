51d162de2532a34f8d1bfcce8e1adb1717db9a42
sagaralmad2297 <sagaralmad1997@gmail.com>
2023-08-01 17:23:46 +0530

    added home page with nodemon

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.set('views','./views')
+app.set('view engine','ejs')
+
+app.get('/home',(req,res)=>{
+res.render('home');
+})
 
 const PORT=process.env.PORT
 
