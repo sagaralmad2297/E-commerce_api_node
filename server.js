1fe8236d24420e2370aa90d3792667a7c40bd141
sagaralmad2297 <sagaralmad1997@gmail.com>
2023-08-01 15:41:35 +0530

    configured sequelize using cli

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+require('dotenv').config();
+
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 const PORT=process.env.PORT
+
 app.listen(PORT,()=>{
     console.log(`server started on port ${PORT}`);
 })
